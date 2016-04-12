38d23e792b2bf83b171c4450713b746b9ca23521
Anthony Gold <anthgold@gmail.com>
2016-04-11 21:08:26 -0700

    reworks business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,28 @@
 // Business Logic
 
-  // Creates object Pizza with properties for size and toppings
-function Pizza(pieSize, toppings, price) { // this could also be blank?!
+function Pizza(pieSize, toppings) {
 this.pieSize = pieSize;
-this.toppings = toppings; // this could also be a function
-this.price = price; // adds third element for total price
+this.toppings = [];
 }
 
-  // This prototype will calculate the total price of the pie
-Pizza.prototype.totalPrice = function(something) { // this needs help
-  //  totalPrice is sumOfToppings * sizeMultiplier + priceOfSize
-  var priceSmall = 10;
-  var priceMedium = 15;
-  var priceLarge = 20;
-  return this.price ~the operation~ ;
-}
-
-  // This prototype will calculate the price of toppings
-Pizza.prototype.addTurn = function(roll1, roll2) { // model
-  return this.turnScore + roll1 + roll2;
-}
-Pizza.prototype.addToppings = function(toppingsArray) { // this needs help
-  //  toppingsPrice is priceTopping1 + ... priceTopping(n) -- fracking loop?
-  var  priceMushrooms = 0.5;
-  var  priceExtraCheese = 1;
-  var  pricePepperoni = 2;
-  var multiplierSmall = 1;
-  var multiplierMedium = 1.5;
-  var multiplierLarge = 2;
-  return this.toppings + toppingsArray[0] + ... toppingsArray[n];
-}
+Pizza.prototype.price = function () {
+  var totalPrice = 0;
+  if (this.size === "small") {
+    totalPrice = 10;
+  } else if (this.size === "medium") {
+    totalPrice = 15;
+  } else {
+    totalPrice = 20;
+  }
+  for (i = 0; i <= this.toppings.length; i++) {
+    if this.toppings.length <= 1 {
+      totalPrice
+    } else {
+      total += this.toppings.length * 1;
+    }
+  }
+  return totalPrice
+};
 
 // User Interface Logic
 $(document).ready(function() {
